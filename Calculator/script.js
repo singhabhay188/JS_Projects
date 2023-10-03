6e7fce913a258b434023a4a5dcdd978619bf1d6d
Betu Singh <singhbetu188@gmail.com>
2023-10-03 20:28:25 +0530

    Calculator is Ready

--- Calculator/script.js
+++ Calculator/script.js
@@ -1,6 +1,28 @@
 let expression = document.getElementById('expression')
 
+function clearLastInput(){
+    expression.value = expression.value.substring(0,expression.value.length-1);
+}
 
 function clearInput(){
-    console.log('Clear function is called');
+    expression.value = ''
+}
+
+function enter(symbo){
+    expression.value += symbo;
+}
+
+function solve(){
+    if(expression.value.length==0) return;
+    try{
+       expression.value = eval(expression.value);
+    }
+    catch{
+        let exp = expression.value;
+        expression.value = 'Invalid Syntax';
+        
+        setTimeout(()=>{
+            expression.value = exp;
+        },1000)
+    }
 }
